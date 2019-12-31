ff8c94bd8f6dfb548a1e9748b9eadf53a37e5885
HoangTran <40290572+hoangtran-97@users.noreply.github.com>
2019-12-31 13:14:09 +0200

    feat: typing effect

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@ const sections = document.querySelectorAll(".section");
 const navigationBar = document.getElementById("navigation");
 const navigationFragment = document.createDocumentFragment();
 
+//Navigation List Creation
 sections.forEach((section) => {
     const anchor = document.createElement("a");
     const anchorText = document.createTextNode(section.id);
@@ -11,6 +12,7 @@ sections.forEach((section) => {
     navigationFragment.appendChild(anchor);
 })
 navigationBar.appendChild(navigationFragment);
+
 const navigationBanner = document.getElementById("navigation-banner")
 const navigationInfo = document.getElementById("navigation-info")
 const navigationDestinations = document.getElementById("navigation-destinations")
@@ -18,10 +20,13 @@ const navigationReview = document.getElementById("navigation-review")
 const navigationContact = document.getElementById("navigation-contact")
 
 const banner = document.getElementById("banner");
-const info = document.getElementById("info");
+window.innerWidth > 900 ? info = document.getElementById("info") : info = document.getElementById("info-intro-card")
 const destinations = document.getElementById("destinations");
 const review = document.getElementById("review");
 const contact = document.getElementById("contact");
+
+const activeInfo = document.getElementById("info-active")
+const activeDestinations = document.getElementById("destinations-active")
 const dectectFocus = () => {
     //Banner
     const bannerPosition = banner.getBoundingClientRect();
@@ -40,9 +45,11 @@ const dectectFocus = () => {
     const infoVisible = (infoTop >= 0) && (infoBottom <= window.innerHeight);
     infoVisible ?
         (
-            navigationInfo.classList.add("navigation-visible"))
+            navigationInfo.classList.add("navigation-visible"),
+            activeInfo.classList.add("animated-text"))
         : (
-            navigationInfo.classList.remove("navigation-visible"))
+            navigationInfo.classList.remove("navigation-visible"),
+            activeInfo.classList.remove("animated-text"))
     //Destinations
     const destinationsPosition = destinations.getBoundingClientRect();
     const destinationsTop = destinationsPosition.top;
@@ -50,9 +57,11 @@ const dectectFocus = () => {
     const destinationsVisible = (destinationsTop >= 0) && (destinationsBottom <= window.innerHeight);
     destinationsVisible && !infoVisible ?
         (
-            navigationDestinations.classList.add("navigation-visible"))
+            navigationDestinations.classList.add("navigation-visible"),
+            activeDestinations.classList.add("animated-text"))
         : (
-            navigationDestinations.classList.remove("navigation-visible"))
+            navigationDestinations.classList.remove("navigation-visible"),
+            activeDestinations.classList.remove("animated-text"))
     //Review
     const reviewPosition = review.getBoundingClientRect();
     const reviewTop = reviewPosition.top;
